8bd3842b87a2cc663ae260d6c28b5110d8af4191
Julien Rouhaud <julien.rouhaud@free.fr>
2019-02-02 13:23:47 +0100

    Add PostgreSQL 12 compatibility.
    578b229718e (remove WITH OIDS support) changed CreateTemplateTupleDesc()
    signature.

--- pg_wait_sampling.c
+++ pg_wait_sampling.c
@@ -380,7 +380,11 @@ pg_wait_sampling_get_current(PG_FUNCTION_ARGS)
 		params->ts = GetCurrentTimestamp();
 
 		funcctx->user_fctx = params;
+#if PG_VERSION_NUM >= 120000
+		tupdesc = CreateTemplateTupleDesc(4);
+#else
 		tupdesc = CreateTemplateTupleDesc(4, false);
+#endif
 		TupleDescInitEntry(tupdesc, (AttrNumber) 1, "pid",
 						   INT4OID, -1, 0);
 		TupleDescInitEntry(tupdesc, (AttrNumber) 2, "type",
@@ -585,7 +589,11 @@ pg_wait_sampling_get_profile(PG_FUNCTION_ARGS)
 		funcctx->max_calls = profile->count;
 
 		/* Make tuple descriptor */
+#if PG_VERSION_NUM >= 120000
+		tupdesc = CreateTemplateTupleDesc(5);
+#else
 		tupdesc = CreateTemplateTupleDesc(5, false);
+#endif
 		TupleDescInitEntry(tupdesc, (AttrNumber) 1, "pid",
 						   INT4OID, -1, 0);
 		TupleDescInitEntry(tupdesc, (AttrNumber) 2, "type",
@@ -703,7 +711,11 @@ pg_wait_sampling_get_history(PG_FUNCTION_ARGS)
 		funcctx->max_calls = history->count;
 
 		/* Make tuple descriptor */
+#if PG_VERSION_NUM >= 120000
+		tupdesc = CreateTemplateTupleDesc(5);
+#else
 		tupdesc = CreateTemplateTupleDesc(5, false);
+#endif
 		TupleDescInitEntry(tupdesc, (AttrNumber) 1, "pid",
 						   INT4OID, -1, 0);
 		TupleDescInitEntry(tupdesc, (AttrNumber) 2, "sample_ts",
