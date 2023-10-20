b8037cf0885a54d08132d0e93d943b642b9cb1df
Sergey Shinderuk <s.shinderuk@postgrespro.ru>
2023-10-20 16:06:41 +0300

    Build and test on Travis without Docker
    Install postgresql packages from the PGDG apt repo.

---
+++ run-tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+
+PATH=/usr/lib/postgresql/$PG_MAJOR/bin:$PATH
+export PGDATA=/var/lib/postgresql/$PG_MAJOR/test
+export COPT=-Werror
+export USE_PGXS=1
+
+sudo chmod 1777 /var/lib/postgresql/$PG_MAJOR
+sudo chmod 1777 /var/run/postgresql
+
+make clean
+make
+
+sudo -E env PATH=$PATH make install
+
+initdb
+echo "shared_preload_libraries = pg_wait_sampling" >> $PGDATA/postgresql.conf
+
+pg_ctl -l logfile start
+make installcheck
+pg_ctl stop
