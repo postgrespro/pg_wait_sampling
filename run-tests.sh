0206f69a2c1387b2a9648cca9eaedf659462a62e
Sergey Shinderuk <s.shinderuk@postgrespro.ru>
2025-06-21 13:24:22 +0300

    add mkdir

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,7 @@ export PGDATA=/var/lib/postgresql/$PG_MAJOR/test
 export COPT=-Werror
 export USE_PGXS=1
 
+sudo mkdir -p /var/lib/postgresql/$PG_MAJOR
 sudo chmod 1777 /var/lib/postgresql/$PG_MAJOR
 sudo chmod 1777 /var/run/postgresql
 
